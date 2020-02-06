84d1f053030b29b02be8f1592ef642cfe405be1b
Student Name <tet@test.com>
2020-02-06 11:36:53 -0500

    Added services and routing

--- src/app/albums/album-list/album-list.component.ts
+++ src/app/albums/album-list/album-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { Album } from "../album.model";
 import { ALBUMS } from "../albums.data";
+import { AlbumService } from "../shared/album.service"
+import { Observable } from 'rxjs';
 
 @Component({
   selector: "app-album-list",
@@ -8,11 +10,17 @@ import { ALBUMS } from "../albums.data";
   styleUrls: ["./album-list.component.css"]
 })
 export class AlbumListComponent implements OnInit {
-  albumsArray: Album[];
+  albumsArray: Observable<Album[]>;
   clickedAlbum: string;
+
+  constructor(private albumService: AlbumService) { };
+
   ngOnInit(): void {
-    this.albumsArray = ALBUMS;
+    this.getAlbums();
    }
+   getAlbums() {
+    this.albumsArray = this.albumService.getAlbums();
+  }
 
   parentFunctionHandler(album) {
     alert(
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AlbumListComponent } from './albums/album-list/album-list.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/welcome", pathMatch: "full" },
+  { path: "welcome", component: WelcomeComponent },
+  { path: "about", component: AboutComponent},
+  { path: "albums", component: AlbumListComponent },
+  {path: "**", component: NotfoundComponent, pathMatch: "full"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+
+
+ }
