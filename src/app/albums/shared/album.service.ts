8db186442300dacfba2b599e6096193f74342fd5
Student Name <tet@test.com>
2020-02-07 09:19:21 -0500

    Fixing bugs

--- src/app/albums/shared/album.service.ts
+++ src/app/albums/shared/album.service.ts
@@ -8,7 +8,7 @@ import { Album } from '../album.model';
 providedIn: 'root'
 })
 export class AlbumService {
-     url = "http://localhost:4445/albums";
+     url = "http://localhost:3334/albums";
 
      constructor(private http: HttpClient) { }
 
